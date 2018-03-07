8e430c58562272204bbd9e33e14191784bb904f1
KAR16 <kherrera16@gmail.com>
2018-03-07 10:36:13 -0600

    Se añadió el proyecto de rama desarrollador

---
+++ models/Encuesta.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+
+var EncuestaSchema = new mongoose.Schema({
+    PouchDBId: String,
+    LastUpdatedDateTime: Number,
+    EntityType:String,
+    isSync: Boolean,
+    isDeleted: Boolean,
+    preguntas: { type: Array, "default": [] },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+mongoose.model('Encuesta', EncuestaSchema);
